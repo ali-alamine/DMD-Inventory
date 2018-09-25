6abb1f8681f8216aa9e45fa7a2ec4f1a2722c2e5
hassanhawi <hassanhawi8@gmail.com>
2018-09-25 12:41:48 +0300

    first commit

---
+++ src/assets/api/appx/controllers/stock.php
@@ -0,0 +1,222 @@
+<?php
+require APPPATH . '/libraries/REST_Controller.php';
+class stock extends REST_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('stock_model');
+    }
+    public function stockAcc_post(){
+        $name = $this->post('name');
+        // $cost = $this->post('cost');
+        $price = $this->post('price');
+        // $quantity = $this->post('quantity');
+        $bar_code = $this->post('bar_code');
+        $type = "AC";
+        $this->db->trans_start();
+        $this->db->trans_strict(FALSE);
+        $IID = $this->stock_model->add('item',array("name" => $name, "price_item" => $price,
+        "bar_code" => $bar_code,"type" => $type));
+        if ($IID === 0) {
+            $this->response("Item information could not be saved. Try again.", 404);
+        } else {
+            // $result = $this->stock_model->add('accessories',array("IID" => $IID,"cost" => $cost, "price" => $price,
+            // "quantity" => $quantity));
+            $result = $this->stock_model->add('accessories',array("IID" => $IID, "price" => $price,));
+        }
+        $this->db->trans_complete();
+        if ($this->db->trans_status() === FALSE) {
+            # Something went wrong.
+            $this->db->trans_rollback();
+            return FALSE;
+        } 
+        else {
+            # Everything is Perfect. 
+            # Committing data to the database.
+            $this->db->trans_commit();
+            return TRUE;
+        }
+    } 
+    public function stockMRC_post(){
+        $name = $this->post('name');
+        // $quantity = $this->post('quantity');
+        // $cost = $this->post('cost');
+        $price = $this->post('price');
+        $bar_code = $this->post('bar_code');
+        $company = $this->post('company');
+        $type = "RC";
+        $this->db->trans_start();
+        $this->db->trans_strict(FALSE);
+        $IID = $this->stock_model->add('item',array("name" => $name, "price_item" => $price,
+        "bar_code" => $bar_code,"type" => $type));
+        if ($IID === 0) {
+            $this->response("Item information could not be saved. Try again.", 404);
+        } else {
+            // $result = $this->stock_model->add('recharge_card',array("IID" => $IID,"company" => $company,"cost" => $cost, "price" => $price,
+            // "quantity" => $quantity));
+            $result = $this->stock_model->add('recharge_card',array("IID" => $IID,"company" => $company, "price" => $price));
+        }
+        $this->db->trans_complete();
+        if ($this->db->trans_status() === FALSE) {
+            # Something went wrong.
+            $this->db->trans_rollback();
+            return FALSE;
+        } 
+        else {
+            # Everything is Perfect. 
+            # Committing data to the database.
+            $this->db->trans_commit();
+            return TRUE;
+        }
+    } 
+    public function stockOF_post(){
+        $name = $this->post('name');
+        $num_of_mounth = $this->post('num_of_mounth');
+        $num_of_credit = $this->post('num_of_credit');
+        $price = $this->post('price');
+        $bar_code = $this->post('bar_code');
+        $company = $this->post('company');
+        $type = "OF";
+        $this->db->trans_start();
+        $this->db->trans_strict(FALSE);
+        $IID = $this->stock_model->add('item',array("name" => $name, "price_item" => $price,
+        "bar_code" => $bar_code,"type" => $type));
+        if ($IID === 0) {
+            $this->response("Item information could not be saved. Try again.", 404);
+        } else {
+            $result = $this->stock_model->add('offers',array("IID" => $IID,"company" => $company,"num_of_mounth" => $num_of_mounth,"num_of_credit" => $num_of_credit, "price" => $price));
+        }
+        $this->db->trans_complete();
+        if ($this->db->trans_status() === FALSE) {
+            # Something went wrong.
+            $this->db->trans_rollback();
+            return FALSE;
+        } 
+        else {
+            # Everything is Perfect. 
+            # Committing data to the database.
+            $this->db->trans_commit();
+            return TRUE;
+        }
+    }
+
+    public function stockAcc_put(){
+        $name = $this->put('name');
+        // $quantity = $this->put('quantity');
+        // $cost = $this->put('cost');
+        $price = $this->put('price');
+        $bar_code = $this->put('bar_code');
+        $IID = $this->put('IID');
+        $this->db->trans_start();
+        $this->db->trans_strict(FALSE);
+        $resultAcc = $this->stock_model->update('accessories',$IID, array( "price" => $price));
+        // $resultAcc = $this->stock_model->update('accessories',$IID, array( "cost" => $cost,"price" => $price,
+        // "quantity" => $quantity));
+        $resultItem = $this->stock_model->update('item',$IID, array("name" => $name, "bar_code" => $bar_code, "price_item" => $price));
+        $this->db->trans_complete();
+        if ($this->db->trans_status() === FALSE) {
+            # Something went wrong.
+            $this->db->trans_rollback();
+            return FALSE;
+        } 
+        else {
+            # Everything is Perfect. 
+            # Committing data to the database.
+            $this->db->trans_commit();
+            return TRUE;
+        }
+    }
+    public function stockMRC_put(){
+        // $is_offers = $this->put('is_offers');
+        $name = $this->put('name');
+        // $cost = $this->put('cost');
+        $price = $this->put('price');
+        // $quantity = $this->put('quantity');
+        $bar_code = $this->put('bar_code');
+        $IID = $this->put('IID');
+        $this->db->trans_start();
+        $this->db->trans_strict(FALSE);
+        $resultRC = $this->stock_model->update('recharge_card',$IID, array("price" => $price));
+        // $resultRC = $this->stock_model->update('recharge_card',$IID, array( "cost" => $cost,"price" => $price,
+        // "quantity" => $quantity));
+        $resultItem = $this->stock_model->update('item',$IID, array("name" => $name, "bar_code" => $bar_code, "price_item" => $price));
+        $this->db->trans_complete();
+        if ($this->db->trans_status() === FALSE) {
+            # Something went wrong.
+            $this->db->trans_rollback();
+            return FALSE;
+        } 
+        else {
+            # Everything is Perfect. 
+            # Committing data to the database.
+            $this->db->trans_commit();
+            return TRUE;
+        }
+    }
+    public function stockOF_put(){
+        $name = $this->put('name');
+        $num_of_mounth = $this->put('num_of_mounth');
+        $num_of_credit = $this->put('num_of_credit');
+        $price = $this->put('price');
+        $bar_code = $this->put('bar_code');
+        $IID = $this->put('IID');
+        $this->db->trans_start();
+        $this->db->trans_strict(FALSE);
+        $resultRC = $this->stock_model->update('offers',$IID, array( "num_of_mounth" => $num_of_mounth,"num_of_credit" => $num_of_credit,"price" => $price));
+        $resultItem = $this->stock_model->update('item',$IID, array("name" => $name, "bar_code" => $bar_code, "price_item" => $price));
+        $this->db->trans_complete();
+        if ($this->db->trans_status() === FALSE) {
+            # Something went wrong.
+            $this->db->trans_rollback();
+            return FALSE;
+        } 
+        else {
+            # Everything is Perfect. 
+            # Committing data to the database.
+            $this->db->trans_commit();
+            return TRUE;
+        }
+    }
+    public function stockCT_put(){
+        $credits = $this->put('credits');
+        $IID = $this->put('IID');
+        $result = $this->stock_model->update('credit',$IID, array( "credits" => $credits));
+        if ($result === 0) {
+            $this->response("update Credits information could not be saved. Try again.", 404);
+        } else {
+            $this->response($result, 200);
+        }
+    }
+    // public function deleteItem_put(){
+    //     $id = $this->put('ID');
+    //     $type = $this->put('type');
+    //     if (!$id) {
+    //         $this->response("Parameter missing", 404);
+    //     }
+    //     if($type=="AC"){
+    //         if ($this->stock_model->delete('accessories',$id))
+    //             $this->response("Success", 200);
+    //     } else
+    //     if($type=="RC"){
+
+    //     } else
+    //     if($type=="CT"){
+
+    //     } else
+    //     if($type=="OF"){
+
+    //     }else {
+    //         $this->response("Cannot Delete this item, try to delete its items", 400);
+    //     }
+    // }
+    public function searchItem_get(){
+        $searchInput = $this->get('searchInput');
+        $result = $this->stock_model->searchItem($searchInput);
+        if ($result === 0) {
+            $this->response("Item information could not be saved. Try again.", 404);
+        } else {
+            $this->response($result, 200);
+        }
+    }    
+}
---
+++ src/assets/api/appx/controllers/subscriber.php
@@ -0,0 +1,135 @@
+<?php
+ini_set('max_execution_time', 0); 
+ini_set('memory_limit','6028M');
+require APPPATH . '/libraries/REST_Controller.php';
+class subscriber extends REST_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('subscriber_model');
+    }     
+    public function subscriber_post()
+    {
+        $name = $this->post('name');
+        $phone = $this->post('phone');
+        $address = $this->post('address');
+        $profile = $this->post('profile');
+        $result = $this->subscriber_model->add(array("name" => $name, "phone" => $phone, "address" => $address, "profile" => $profile));
+
+        if ($result === 0) {
+            $this->response("Client information could not be saved. Try again.", 404);
+        } else {
+            $this->response("success", 200);
+        }
+    }   
+
+    public function newSubscription_post()
+    {
+        $profile = $this->post('profile');
+        $subID = $this->post('subID');
+        $isPaid = $this->post('isPaid');
+        $expDate = $this->post('expDate');
+        $subDate = $this->post('subDate');        
+        if($isPaid){
+            date_default_timezone_set("Asia/Beirut");
+            $paymentDate=date("Y-m-d H:i:s");
+        }
+        else{
+            $paymentDate='';
+        }
+        $result = $this->subscriber_model->addSubscription(array("SBID" => $subID, "sub_date" => $subDate, "payment_date" => $paymentDate, "exp_date" => $expDate, "profile" => $profile, "is_paid" => $isPaid));
+
+        if ($result === 0) {
+            $this->response("Client information could not be saved. Try again.", 404);
+        } else {
+            $this->response("success", 200);
+        }
+
+    }
+
+    public function getMonths_get()
+    {
+        $subscriberID = $this->get('subscriberID');
+        $result = $this->subscriber_model->getMonths($subscriberID);
+        if ($result) {
+            $this->response($result, 200);
+
+            exit;
+        }
+    }
+
+    
+
+    public function subscriber_put()
+    {
+        $subscriber_name = $this->put('name');
+        $subscriber_phone = $this->put('phone');
+        $subscriber_address = $this->put('address');
+        $subscriber_profile = $this->put('profile');
+        $subscriberID = $this->put('id');
+
+        $result = $this->subscriber_model->update($subscriberID, array("name" => $subscriber_name, "phone" => $subscriber_phone, "address" => $subscriber_address, "profile" => $subscriber_profile));
+        if ($result === 0) {
+            $this->response("subscriber information could not be saved. Try again.", 404);
+        } else {
+            $this->response("success", 200);
+        }
+    }
+
+    public function enableDisableSub_put()
+    {
+        
+        $subscriberID = $this->put('id');
+        $result = $this->subscriber_model->toggleActivation($subscriberID);
+        if ($result === 0) {
+            $this->response("subscriber information could not be saved. Try again.", 404);
+        } else {
+            $this->response("success", 200);
+        }
+    }
+
+    public function autoSubscription_get(){
+        $result = $this->subscriber_model->autoSubscription();
+        if ($result) {
+            $this->response($result, 200);
+        } else {
+            $this->response("Error", 404);
+        }
+    }
+
+    public function setUnsetPayment_put()
+    {        
+        $subDetailsID = $this->put('id');
+        $isPaid = $this->put('isPaid');
+        $result;
+        if($isPaid){
+            $result = $this->subscriber_model->setUnpaid($subDetailsID);
+        }
+        else{
+            $result = $this->subscriber_model->setPaid($subDetailsID);
+        }
+        
+        if ($result === 0) {
+            $this->response("subscriber information could not be saved. Try again.", 404);
+        } else {
+            $this->response("success", 200);
+        }
+    }
+
+    public function deleteSubscription_put()
+    {
+        
+        $subDetailsID = $this->put('id');
+        $result = $this->subscriber_model->deleteSubscription($subDetailsID);
+        if ($result === 0) {
+            $this->response("subscriber information could not be saved. Try again.", 404);
+        } else {
+            $this->response("success", 200);
+        }
+    }
+    
+
+    
+    
+}
